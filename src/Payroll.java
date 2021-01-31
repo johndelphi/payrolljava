88650fd760e9058fbdfaa51f20430a338d6a9da1
John.michael <John.michael@studentambassadors.com>
2021-01-31 17:54:34 -0600

    renamed payrate

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,5 +1,23 @@
-public class Payroll {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class  Payroll {
+      private String name;
+      private String Id;
+      private double hours;
+      private double Payrate;
+
+      public Payroll( String N,String id, double hrs,double rte ){
+          name=N;
+          Id=id;
+          hours=hrs;
+          Payrate=rte;
+          
+      }
+      /*
+      * The set Name method which accepts an argument
+      *which is stored in the naem field 
+      *@ parameter 5 */
+
+     public static void main(String[] args) throws Exception {
+
+        
     }
 }
