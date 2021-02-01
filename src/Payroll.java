c551d00de341517e01d2a6adff3e664a0842017d
John.michael <John.michael@studentambassadors.com>
2021-01-31 18:48:27 -0600

    added new getters and setter

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,11 +1,11 @@
 public class  Payroll {
-      private String name;
+      private String Name;
       private String Id;
       private double hours;
       private double Payrate;
-
-      public Payroll( String N,String id, double hrs,double rte ){
-          name=N;
+      // field constructor
+      public Payroll( String name,String id, double hrs,double rte ){
+          name=Name;
           Id=id;
           hours=hrs;
           Payrate=rte;
@@ -14,7 +14,7 @@ public class  Payroll {
       /*
       * The set Name method which accepts an argument
       *which is stored in the naem field 
-      *@ parameter 5 */
+      *@ parameter  */
 
      public static void main(String[] args) throws Exception {
 
